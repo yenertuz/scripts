5130017d8a688b7a92bd46a5986db9b8b4a4d09f
Yener Tuz <32311607+yenertuz@users.noreply.github.com>
2019-03-19 14:27:29 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ npm install @babel/core @babel/preset-env @babel/preset-react babel-loader react
 curl https://www.yenertuz.com/scripts/webpack.config.js > webpack.config.js && \
 curl https://www.yenertuz.com/scripts/index.html > index.html && \
 curl https://www.yenertuz.com/scripts/entry.jsx > entry.jsx &&
-clear && echo "FRONTEND FOLDER CONFIGURED"
+echo -e "\e[1;32m yenertuz.com: Successfully created frontend project\e[0m"
