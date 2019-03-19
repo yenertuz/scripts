7474eb50b819b1cf1cebb1c15e99020e16d8be03
Yener Tuz <32311607+yenertuz@users.noreply.github.com>
2019-03-19 15:21:23 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ echo " " >> README.md && \
 npm install @babel/core @babel/preset-env @babel/preset-react babel-loader react react-dom webpack webpack-cli jquery && \
 curl https://www.yenertuz.com/scripts/webpack.config.js > webpack.config.js && \
 curl https://www.yenertuz.com/scripts/index.html > index.html && \
-curl https://www.yenertuz.com/scripts/entry.jsx > entry.jsx && | 
+curl https://www.yenertuz.com/scripts/entry.jsx > entry.jsx && \
 echo -e "\e[1;32m yenertuz.com: Successfully created frontend project\e[0m"
