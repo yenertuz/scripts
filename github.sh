a48641bb860ec02d5382c1e83b0d73f1a22a95ba
yenertuz <yenertuz@hotmail.com>
2019-03-19 23:45:38 -0700

    update github.sh change echo to printf

--- github.sh
+++ github.sh
@@ -1,2 +1,14 @@
 rm -rRf the_center
 git clone https://github.com/yenertuz/the_center
+FIRST=`cat the_center/README.md`
+curl -s https://www.yenertuz.com/scripts/github.php > del.php 
+php del.php
+rm -rRf del.php 
+SECOND=`cat the_center/README.md`
+cd the_center 
+git add .
+git commit -m "gibberish" 
+git push 
+cd ..
+rm -rRf the_center
+printf "\e[1;32m yenertuz.com: $FIRST changed to $SECOND \n \e[0m"
